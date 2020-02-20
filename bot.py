3d67d6ef526086a51390abb4ddecce0c9f75e0d5
Yerassil <yerassil@gmail.com>
2020-02-20 20:05:38 +0600

    Initial commit. Add simple echo functionality

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def talk(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+#  RUN
+bot.polling(none_stop=True)
